20f35b9f05135d853413d96f2a927f841401cac0
Liam Cavens <liamcavens@googlemail.com>
2018-04-02 18:56:09 +0100

    Done 13/13, was stuck on doing the rest of the extension

--- codeclan_class.rb
+++ codeclan_class.rb
@@ -5,6 +5,7 @@ class Student
     def initialize(name, cohert)
         @name = name
         @cohert = cohert   
+
     end
 
     def talk()
@@ -13,5 +14,61 @@ class Student
 
     def say_fav_language(language)
         return "My favourite language is #{language}"
+    end
+end
+
+### PART B OF HOMEWORK
+
+class SportsTeam
+
+    attr_accessor :team_name, :players, :coach, :points
+
+    def initialize(team_name, players, coach, points)
+        @team_name = team_name
+        @players = players
+        @coach = coach
+        @points = points
+    end
+
+    def add_new_player(new_player)
+        @players << new_player
+    end
+
+    def find_player_by_name(player_name)
+        for player in @players
+            if player_name == player
+                return true
+            end
+        end
+        return nil
+    end
+
+    def add_win(num)
+            @points += (num)  
+    end
+
+    def add_lose(num_lose)
+            @points += (num_lose)
+    end
+end
+
+### Extension
+
+class Library
+
+    attr_reader :title, :student_name, :date_of_pickup
+
+    def initialize(title, student_name, date_of_pickup)
+        @title = title
+        @student_name = student_name
+        @date_of_pickup = date_of_pickup
+    end
+
+    def book_info(title)
+        if @title == title
+            return "#{title}, #{student_name}, #{date_of_pickup}"
+        else
+            "Error"
+        end
     end
 end
--- codeclan_class_specs.rb
+++ codeclan_class_specs.rb
@@ -33,3 +33,55 @@ class TestCodeClanCohert < MiniTest::Test
     assert_equal("My favourite language is Ruby", @student.say_fav_language("Ruby"))
  end
 end
+
+### PART B OF HOMEWORK
+
+class TestSportsTeam < MiniTest::Test
+
+    def setup()
+    @sports_team = SportsTeam.new(
+        "Scotland", 
+        ["Brown", "Fletcher", "Tierney"],
+         "McLeish",
+         0
+         )
+    end
+
+ def test_add_new_player
+        assert_equal(["Brown", "Fletcher", "Tierney","Cavens"], @sports_team.add_new_player("Cavens"))
+ end
+
+ def test_set_coach_name
+    @sports_team.coach = "White"
+    assert_equal("White", @sports_team.coach)
+ end
+
+ def test_find_player_by_name  
+    assert_equal(true, @sports_team.find_player_by_name("Brown"))
+ end
+
+ def test_add_points_for_win
+    assert_equal(3, @sports_team.add_win(3))
+ end
+
+ def test_points_for_lose
+    assert_equal(-3, @sports_team.add_lose(-3))
+ end
+end
+
+### Extension
+
+class TestLibrary < MiniTest::Test
+
+   def setup()
+    @book = Library.new("Lord of the Rings", "Jeff", "01/12/16")
+   end
+
+   def test_name_of_book
+       assert_equal("Lord of the Rings", @book.title)
+   end
+
+   def test_information_of_book
+       assert_equal("Lord of the Rings, Jeff, 01/12/16", @book.book_info("Lord of the Rings"))
+   end
+end
